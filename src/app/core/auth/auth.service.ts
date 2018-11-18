15e4c2836beb9bfce7af01d42d7db205a083e234
Paulo Tosi <tosi.paulo@gmail.com>
2018-11-18 13:03:17 -0200

    Criando serviço para setar token localstorage

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators'
 
 import { environment } from '../../../environments/environment'
+import { TokenService } from '../token/token.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService) { }
 
   authenticate( email: string, password: string) {
 
-    return this.http.post(`${environment.api}/auth/authenticate`, {email, password})
+    return this.http
+      .post(`${environment.api}/auth/authenticate`, {email, password})
+      .pipe(tap(res => {
+        const authToken = res['token'];
+        this.tokenService.setToken(authToken);
+      }))
 
   }
 }
